de43119ac89d40887bb78b8329deb00379d2542b
PanchonSoto <panchon.ues@gmail.com>
2024-05-15 12:17:51 -0700

    Producto endpoint

---
+++ CafeBackend/Controllers/ProductosController.cs
@@ -0,0 +1,73 @@
+﻿using CafeBackend.Data;
+using CafeBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ProductosContext _context;
+
+        public ProductosController(ProductosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProducto(int id, Producto producto)
+        {
+            if(id!=producto.productoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) 
+            {
+                if (!ProductoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        //delete
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCerveza(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            _context.Productos.Remove(producto);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return _context.Productos.Any(e=>e.productoId==id);
+        }
+    }
+}
---
+++ CafeBackend/Models/Productos.cs
@@ -0,0 +1,13 @@
+﻿namespace CafeBackend.Models
+{
+    public class Producto
+    {
+        public int productoId { get; set; }
+        public required string nombre { get; set; }
+        public required string descripcion { get; set; }
+        public decimal precio { get; set; }
+        public Boolean disponible { get; set; }
+        public DateTime fechaCreacion { get; set; } = DateTime.Now;
+
+    }
+}
