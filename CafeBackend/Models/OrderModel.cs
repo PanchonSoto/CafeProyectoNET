88ca2f6df95ac3a0ad20f82ba6149b3923ea1678
PanchonSoto <panchon.ues@gmail.com>
2024-05-20 06:43:50 -0700

    get/create order by leftjoinsql

---
+++ CafeBackend/Models/OrderModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CafeBackend.Models
+{
+    public class OrderInsertModel
+    {
+        public int OrdenId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public int ProductoId { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime FechaOrden { get; set; }
+    }
+
+    public class OrderSelectModel
+    {
+        public int OrdenId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public int ProductoId { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime FechaOrden { get; set; }
+        public string NombreProducto { get; set; }
+        public string DescripcionProducto { get; set; }
+    }
+}
